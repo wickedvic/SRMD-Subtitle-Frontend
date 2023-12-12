3534984469e59408f407b437e437385abdf89023
Vivek Sanghvi <viksipart@gmail.com>
2023-12-12 23:11:30 +0530

    Latest changes to UI and functionality

--- src/components/Register.js
+++ src/components/Register.js
@@ -3,8 +3,6 @@ import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@mui/material/TextField";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
 import Link from "@mui/material/Link";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
@@ -71,6 +69,7 @@ export default function Register(props) {
       }).then((result) => {
         axios
           .post("https://speechtotexteditor.azurewebsites.net/api/v1/users", {
+            userType: "user",
             firstName: firstName,
             lastName: lastName,
             email: email,
