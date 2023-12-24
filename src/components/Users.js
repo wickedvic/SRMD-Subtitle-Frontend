1b771a4f941a966ece7326e99450b5a6f41dc712
nidhishah-seva <149015696+nidhishah-seva@users.noreply.github.com>
2023-12-24 10:40:53 +0530

    Update Users.js

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,14 +1,7 @@
-import React, { useState, useEffect } from "react";
 import axios from "axios";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
-import moment from "moment";
 import { MaterialReactTable } from "material-react-table";
+import moment from "moment";
+import React, { useEffect, useState } from "react";
 import "../App.css";
 
 const Users = () => {
